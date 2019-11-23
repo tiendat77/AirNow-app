1e285b27af778ee62957e2f127fbb4f6edf630cc
JiWoo <huynhztienzdat@gmail.com>
2019-11-23 12:45:31 +0700

    Initial commit

---
+++ app/src/main/java/uit/thesis/airnow/Constants.java
@@ -0,0 +1,9 @@
+package uit.thesis.airnow;
+
+public class Constants {
+
+    public static String SERVER_API_FORCAST = "http://13.59.35.198:8000/api/forecast";
+    public static String SERVER_API_LOCATION = "http://13.59.35.198:8000/api/locations";
+    public static String SERVER_API_CHART_DATA = "http://13.59.35.198:8000/api/airdata";
+
+}
