097277c2788e5a11859da9d21b21e1ef65978d32
JiWoo <huynhztienzdat@gmail.com>
2019-11-29 01:10:25 +0700

    ListView template

--- app/src/main/java/uit/thesis/airnow/Mock.java
+++ app/src/main/java/uit/thesis/airnow/Mock.java
@@ -1,7 +1,16 @@
 package uit.thesis.airnow;
 
-public final class Mock {
+import uit.thesis.airnow.model.Forecast;
 
-    public String URL_TEST = "abcxyz";
+public class Mock {
+
+    public static String forecastJson = "{}";
+
+    public static Forecast[] forecasts = new Forecast[] {
+        new Forecast("Kyoto", 7, 12, 3, 52, "Good"),
+        new Forecast("Singapore", 27, 54, 13.8, 78, "Moderate"),
+        new Forecast("Shenyang", -11, 149, 55, 61, "Unhealthy for sensitive group"),
+        new Forecast("Krakow", 8, 171, 95, 57, "Unhealthy"),
+    };
 
 }
