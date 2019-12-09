cb2883bb73911296fb03b4f8a0bf3afee885527f
JiWoo <huynhztienzdat@gmail.com>
2019-12-09 17:16:16 +0700

    Merge smartconfig - fix crash app

--- app/src/main/java/uit/thesis/airnow/ui/home/HomeFragment.java
+++ app/src/main/java/uit/thesis/airnow/ui/home/HomeFragment.java
@@ -11,7 +11,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
